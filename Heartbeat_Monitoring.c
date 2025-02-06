eb3ba075c8b13d6ca76629a7d3712466dc8f6817
Hatem-707 <hatemfeckry347@gmail.com>
2025-02-06 15:53:02 +0200

    modified the can recieve callback function to suppot the rest of data types

--- Heartbeat_Monitoring.c
+++ Heartbeat_Monitoring.c
@@ -13,12 +13,96 @@
 #define FREQ_CHECK_INTERVAL 1000
 #define BUS_WARNING_THRESHOLD 1000
 
+#define HEARTBEAT_1_HEX         0x01
+#define HEARTBEAT_2_HEX         0x02
+#define HEARTBEAT_3_HEX         0x03
+#define HEARTBEAT_4_HEX         0x04
+
+
+#define IMU_HEX                 0x05
+
+#define THRUSTER_CURRENT_1_HEX  0x06
+#define THRUSTER_CURRENT_2_HEX  0x07
+#define THRUSTER_CURRENT_3_HEX  0x08
+#define THRUSTER_CURRENT_4_HEX  0x09
+#define THRUSTER_CURRENT_5_HEX  0x0A
+#define THRUSTER_CURRENT_6_HEX  0x0B
+
+#define THRUSTER_PWM_1_HEX      0x0C
+#define THRUSTER_PWM_2_HEX      0x0D
+#define THRUSTER_PWM_3_HEX      0x0E
+#define THRUSTER_PWM_4_HEX      0x0F
+#define THRUSTER_PWM_5_HEX      0x10
+#define THRUSTER_PWM_6_HEX      0x11
+
+#define INDICATOR_1_HEX         0x12
+#define INDICATOR_2_HEX         0x13
+#define INDICATOR_3_HEX         0x14
+#define INDICATOR_4_HEX         0x15
+#define INDICATOR_5_HEX         0x16
+#define INDICATOR_6_HEX         0x17
+
+
+#define ARM_1_HEX               0x1C
+#define ARM_2_HEX               0x1D
+#define ARM_3_HEX               0x1E
+#define ARM_4_HEX               0x1F
+
+#define ROV_DEPTH_HEX           0x20
+
+
 typedef enum {
     DEVICE_OK = 0,
     DEVICE_WARNING = 1,
     DEVICE_ERROR = 2,
     DEVICE_RESTARTED = 3
 } DeviceStatus;
+
+typedef struct {
+    float acc_x;
+    float acc_y;
+    float acc_z;
+    float imu_roll;
+    float imu_pitch;
+    float imu_yaw;
+} Imu;
+
+typedef struct {
+    int id; // Store the ID to know which data is valid
+    bool valid; // Flag to indicate if the data is valid
+    Imu imu;
+    float thruster_current_1;
+    float thruster_current_2;
+    float thruster_current_3;
+    float thruster_current_4;
+    float thruster_current_5;
+    float thruster_current_6;
+    float thruster_pwm_1;
+    float thruster_pwm_2;
+    float thruster_pwm_3;
+    float thruster_pwm_4;
+    float thruster_pwm_5;
+    float thruster_pwm_6;
+    float indicator_1;
+    float indicator_2;
+    float indicator_3;
+    float indicator_4;
+    float indicator_5;
+    float indicator_6;
+    bool heartbeat_1;
+    bool heartbeat_2;
+    bool heartbeat_3;
+    bool heartbeat_4;
+    float connection_percentage_1;
+    float connection_percentage_2;
+    float connection_percentage_3;
+    float connection_percentage_4;
+    float arm_1;
+    float arm_2;
+    float arm_3;
+    float arm_4;
+    float rov_depth;
+} DecodedData;
 
 typedef struct {
     bool arr[QUEUE_SIZE];
@@ -169,6 +253,111 @@ void process_heartbeat(uint8_t sender_id, uint8_t sequence_num) {
     return;    
 }
 
+void process_thruster_current(uint8_t id, float value, DecodedData * current_msg){
+    switch (id)
+    {
+    case 1:
+        current_msg->thruster_current_1 = value;
+        break;
+    case 2:
+        current_msg->thruster_current_2 = value;
+        break;
+    case 3:
+        current_msg->thruster_current_3 = value;
+        break;
+    case 4:
+        current_msg->thruster_current_4 = value;
+        break;
+    case 5:
+        current_msg->thruster_current_5 = value;
+        break;
+    case 6:
+        current_msg->thruster_current_6 = value;
+        break;
+    default:
+        break;
+    }   
+}
+
+void process_thruster_pwm(uint8_t id, float value, DecodedData * current_msg){
+    switch (id)
+    {
+    case 1:
+        current_msg->thruster_pwm_1 = value;
+        break;
+    case 2:
+        current_msg->thruster_pwm_2 = value;
+        break;
+    case 3:
+        current_msg->thruster_pwm_3 = value;
+        break;
+    case 4:
+        current_msg->thruster_pwm_4 = value;
+        break;
+    case 5:
+        current_msg->thruster_pwm_5 = value;
+        break;
+    case 6:
+        current_msg->thruster_pwm_6 = value;
+        break;
+    default:
+        break;
+    }   
+}
+
+void process_indicator(uint8_t id, float value, DecodedData * current_msg){
+    switch (id)
+    {
+    case 1:
+        current_msg->indicator_1 = value;
+        break;
+    case 2:
+        current_msg->indicator_2 = value;
+        break;
+    case 3:
+        current_msg->indicator_3 = value;
+        break;
+    case 4:
+        current_msg->indicator_4 = value;
+        break;
+    case 5:
+        current_msg->indicator_5 = value;
+        break;
+    case 6:
+        current_msg->indicator_6 = value;
+        break;
+    default:
+        break;
+    }   
+}
+
+void process_arm(uint8_t id, float value, DecodedData * current_msg){
+    switch (id)
+    {
+    case 1:
+        current_msg->arm_1 = value;
+        break;
+    case 2:
+        current_msg->arm_2 = value;
+        break;
+    case 3:
+        current_msg->arm_3 = value;
+        break;
+    case 4:
+        current_msg->arm_4 = value;
+        break;
+    default:
+        break;
+    }   
+}
+
+void process_depth(float value, DecodedData* current_msg){
+    current_msg->rov_depth = value;
+}
+void process_imu(Imu value, DecodedData* current_msg){
+    current_msg->imu = value;
+}
+
 void check_heartbeats() {
     for (int i = 0; i < NUM_DEVICES; i++) {
         uint16_t time_since_last = current_time_ms - devices[i].last_heartbeat;
@@ -180,15 +369,81 @@ void check_heartbeats() {
     }
 }
 
-void CAN_Receive_Callback(uint8_t sender_id, uint8_t sequence_num) {
+void CAN_Receive_Callback(uint8_t sender_id, uint8_t* sequence, DecodedData* current_msg) {
     // update_bus_frequency(); 
     switch (sender_id)
     {
-    case 0x01 || 0x02|| 0x03 || 0x04:
-        process_heartbeat(sender_id, sequence_num);
-        break;
-    default:
-        break;
+    case 0x01:
+        case 0x02:
+        case 0x03:
+        case 0x04:
+            process_heartbeat(sender_id, sequence[0]);
+            break;
+
+        // Group: IMU (0x05)
+        case 0x05:
+            Imu imu_data;
+            memcpy(&imu_data, sequence, sizeof(Imu));
+            process_imu(imu_data, current_msg);
+            break;
+
+        // Group: Thruster Currents (0x06 - 0x0B)
+        case 0x06:
+        case 0x07:
+        case 0x08:
+        case 0x09:
+        case 0x0A:
+        case 0x0B:
+            float thruster_current;
+            memcpy(&thruster_current, sequence, sizeof(float));
+            process_thruster_current(sender_id - 5, thruster_current, current_msg);
+            break;
+
+        // Group: Thruster PWM (0x0C - 0x11)
+        case 0x0C:
+        case 0x0D:
+        case 0x0E:
+        case 0x0F:
+        case 0x10:
+        case 0x11:
+            float thruster_pwm;
+            memcpy(&thruster_pwm, sequence, sizeof(float));
+            process_thruster_pwm(sender_id - 0x0B, thruster_pwm, current_msg);
+            break;
+
+        // Group: Indicators (0x12 - 0x17)
+        case 0x12:
+        case 0x13:
+        case 0x14:
+        case 0x15:
+        case 0x16:
+        case 0x17:
+            float indicator;
+            memcpy(&indicator, sequence, sizeof(float));
+            process_indicator(sender_id - 0x11, indicator, current_msg);
+            break;
+
+        // Group: Arms (0x1C - 0x1F)
+        case 0x1C:
+        case 0x1D:
+        case 0x1E:
+        case 0x1F:
+            float arm;
+            memcpy(&arm, sequence, sizeof(float));
+            process_arm(sender_id - 0x1B, indicator, current_msg);
+            break;
+
+
+        // Group: ROV Depth (0x20)
+        case 0x20:
+            float depth;
+            memcpy(&depth, sequence, sizeof(float));
+            process_depth(depth, current_msg);
+            break;
+
+        default:
+            log_warning("Unkown device!",0x00)
+            break;
     }
 }
 
